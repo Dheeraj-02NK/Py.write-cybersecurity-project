ac8fc8cded79a82f6e46b375efcc620ccff1d337
Dheeraj N Kashyap <95096072+Dheeraj-02NK@users.noreply.github.com>
2025-03-05 15:24:19 +0530

    Delete Rev_a_num.py

--- Rev_a_num.py
+++
@@ -1,9 +0,0 @@
-result = ''
-number = input("Enter a number: ")
-
-for i in range(len(number)-1,-1,-1):
-    result = result+number[i]
-
-result = int(result)
-print(int(result))
-print(type(result))
